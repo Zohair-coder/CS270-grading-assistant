dcb109cca54ce33ec49699f96f81edcbbb1e905e
Zohair <zohair.ul.hasan@gmail.com>
2021-07-16 12:48:17 -0400

    created getKey.py template

---
+++ getKey.py
@@ -0,0 +1,13 @@
+import os
+
+def main(key_file):
+    with open(key_file, "r") as f:
+        key_string = f.read()
+    
+    
+
+    pass
+
+if __name__ == "__main__":
+    key_file = "hw3k (2).rkt"
+    main(key_file)
